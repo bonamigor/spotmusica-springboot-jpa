4e0ad37e6d73cc570c56cbe588dced31fcfa859b
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2020-02-28 20:03:56 -0300

    Commit do projeto resultado do curso de springboot da devmedia.

---
+++ intro-spring-boot-web/src/main/java/br/com/devmedia/dao/PlaylistDao.java
@@ -0,0 +1,18 @@
+package br.com.devmedia.dao;
+
+import br.com.devmedia.domain.Playlist;
+
+import java.util.List;
+
+public interface PlaylistDao {
+
+    void salvar(Playlist playlist);
+
+    List<Playlist> recuperar();
+
+    Playlist recuperarPorId(long id);
+
+    void atualizar(Playlist playlist);
+
+    void excluir(long id);
+}
